e6c8006ebb0eef6436c583d6ce2923b3037b71e4
estif <kingestiff@gmail.com>
2025-03-17 09:32:25 +0300

    added authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,3 +71,97 @@ exports.login = async(req,res,next) =>{
         });
     }
 }
+
+exports.protect = async(req,res,next) =>{       
+    let token = '';
+    try{
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')){   
+            token = req.headers.authorization.split(' ')[1];
+        }
+        
+        if(!token){
+            return res.status(401).json({
+                status: "error",
+                message:"Please login to get access!"
+            });
+        }
+
+        const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);    
+
+        const isalive = await User.findById(decoded.id);
+
+        if(!isalive){       
+            return res.status(401).json({
+                status: "error",
+                message:"User no longer exist!"
+            });
+        }
+
+        req.user = isalive;     //(MANDATORY!!)this make data available for our next middleware
+        next();
+
+    }catch(error){
+        if(error.name === 'TokenExpiredError'){
+            return res.status(401).json({
+                status: "error",
+                message:"Token expired please login again"
+            });
+        }
+
+
+        return res.status(401).json({
+            status: "error",
+            message:error.message || "anauthorized access"
+        });
+    }
+
+}
+
+exports.verify = async(req, res, next) => {
+    try{
+        req.user.role = "seller";
+        console.log("User verified successfully");
+        next();
+    }catch(error){
+        res.status(400).json({
+            status: "error",
+            message: "Failed to verfiy user please make sure you have inserted required documents"
+        });
+    }
+}
+
+exports.isUser = (req,res,next) =>{
+    const userRole = req.user.role;
+
+    if(userRole !== 'user'){
+        return res.status(403).json({
+            status: "error",
+            message: "Your not authorized to do this operation",
+        });
+    }
+    next();
+}
+
+exports.isAdmin = (req,res,next) =>{
+    const userRole = req.user.role;
+
+    if(userRole !== 'admin'){
+        return res.status(403).json({
+            status: "error",
+            message: "Your not authorized to do this operation",
+        });
+    }
+    next();
+}
+
+exports.isSeller = (req,res,next) =>{
+    const userRole = req.user.role;
+
+    if(userRole !== 'seller'){
+        return res.status(403).json({
+            status: "error",
+            message: "Your not authorized to do this operation",
+        });
+    }
+    next();
+}
--- routes/autoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {signup, login} = require('../controllers/authController');
-
-router.route('/register').post(signup);
-router.route('/login').post(login);
-
-module.exports = router;
