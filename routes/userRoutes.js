85b0b4ea06131f28e038031c34aec199e8e61db5
estif <kingestiff@gmail.com>
2025-03-29 15:43:47 +0300

    authorization for admin and user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,12 @@ const {protect, verify, isUser} = require('../controllers/authController');
 
 
 router.route('/profile').get(protect, verify, isUser, getUserProfile).patch(protect, verify, isUser, updateUserProfile);
-router.route('/search').get(searchProduct).post(scanBarcode);
-router.route('/notifications').get(getNotification);
+router.route('/search').get(protect, verify, isUser, searchProduct).post(protect, verify, isUser, scanBarcode);
+router.route('/notifications').get(protect, verify, isUser, getNotification);
 router.route('/wishlist').get(protect, verify, isUser, getFromWishlist);
 router.route('/wishlist/:id').post(protect, verify, isUser, addToWishlist);
 router.route('/wishlist/:productId').delete(protect, verify, isUser, removeFromWishlist);
-router.route('/featured-products').get(getFeaturedProducts);
+router.route('/featured-products').get(protect, verify, isUser, getFeaturedProducts);
 
 module.exports =  router;
     
