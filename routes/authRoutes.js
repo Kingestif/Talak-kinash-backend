f130bc922aec4d97e5ded502ff714b7e8a62a344
estif <kingestiff@gmail.com>
2025-03-29 08:28:41 +0300

    updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,6 @@ const uploadMiddleware = require('../middlewares/uploadMiddleware');
 
 router.route('/register').post(uploadMiddleware.single('identification'), signup);
 router.route('/login').post(login);
-router.route('/verify/:token').get(verifyEmail);
+router.route('/verify/:token').post(verifyEmail);
 
 module.exports = router;  
